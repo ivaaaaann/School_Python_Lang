b23a8848e120b78c7804261608edbe28f58525b5
ldh3907 <ldh165163@gmail.com>
2021-05-26 18:19:55 +0900

    add : python code
    add : python code

--- hello.py
+++ hello.py
@@ -1,1 +1,61 @@
-print("hello" * 5)
+# #제곱
+# print(2**2)
+
+# #함수
+# def printMyName(name):
+#     print("Hi", name)
+
+# #함수(포멧 이용)
+# def printMyName1(name):
+#     print("Hi {0}".format(name))
+
+# #함수 호출
+# printMyName("임동현")
+# printMyName1("임동현")
+
+# # 입력(int 형 변환 사용)
+# n1 = int(input())
+# n2 = int(input())
+# print("합 : ", n1+n2)
+
+# n1,n2= map(int, input().split())
+# print("합 : ",n1+n2)
+
+
+# stunum = int(input("학생 수 입력 : "))
+# print("학생수 : ",stunum)
+# #배열
+# height=[]
+# for i in range(0, stunum):
+#     n=int(input("{0}번 학생의 키를 입력하세요 : ".format(i)))
+#     height.append(n)
+
+# print("입력 받은 키 출력\n")
+# for i in height:
+#     print("{0}".format(i))
+
+# #정렬
+# print("제일 키큰 사람 찾기\n")
+# height=sorted(height)
+# for i in height:
+#     print(i)
+
+# arr=["a", "b", "g", "h", "e", "k", "l"]
+# #거꾸로 정렬
+# arr=sorted(arr, reverse=True)
+# print(arr)
+# #배열 거꾸로
+# print(arr[-1])
+
+# stack=[]
+# stack.append(5)
+# stack.append(3)
+# stack.append(2)
+# stack.append(9)
+# #pop 함수
+# print('스택에서 팝(pop)')
+# print(stack.pop())
+# print(stack.pop())
+# print(stack.pop())
+# print(stack.pop())
+
